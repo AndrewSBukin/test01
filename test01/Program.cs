9d04b256ff3132ba5b61856282526a0827e1be18
AndrewSBukin <andrewsbukin@gmail.com>
2016-11-08 11:53:20 +0300

    implemented

--- test01/Program.cs
+++ test01/Program.cs
@@ -25,7 +25,36 @@ namespace test01
     {
         public static List<string[]> Sort(List<string[]> unsorted)
         {
-            return unsorted;
+            if (null == unsorted)
+                throw new ArgumentException("unsorted");
+
+            //create search hashes for start and end points
+            Dictionary<string, int> hashA = new Dictionary<string, int>();
+            HashSet<string> hashB = new HashSet<string>();
+            for (int i = 0; i < unsorted.Count; i++)
+            {
+                hashA[unsorted[i][0]] = i; // N*O(1)
+                hashB.Add(unsorted[i][1]); // N*O(1)
+            }
+            int idx = -1;
+            //search start point
+            for (int i = 0; i < unsorted.Count; i++)
+            {
+                if (!hashB.Contains(unsorted[i][0])) // N*O(1)
+                {
+                    idx = i;
+                    break;
+                }
+            }
+            List<string[]> sorted = new List<string[]>(unsorted.Count);
+            for (int i = 0; i < unsorted.Count; i++)
+            {
+                sorted.Add(unsorted[idx]); // N*O(1)
+                if (i < unsorted.Count-1) // cause last destination point is not in hashA
+                    idx = hashA[unsorted[idx][1]]; // N*O(1)
+            }
+            return sorted;
+            // overall complexity O(N)
         }
     }
 }
