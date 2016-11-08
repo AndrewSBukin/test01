107e31f2426aaffbee3a55fd32d3d4259df6b872
AndrewSBukin <andrewsbukin@gmail.com>
2016-11-08 11:34:00 +0300

    Tests added

--- test01/Program.cs
+++ test01/Program.cs
@@ -10,6 +10,22 @@ namespace test01
     {
         static void Main(string[] args)
         {
+            List<string[]> unsorted = new List<string[]> {
+                new string[] { "Мельбурн", "Кельн" },
+                new string[] { "Москва", "Париж" },
+                new string[] { "Кельн", "Москва" }};
+            List<string[]> sorted = Sorter.Sort(unsorted);
+        }
+
+
+
+    }
+
+    public class Sorter
+    {
+        public static List<string[]> Sort(List<string[]> unsorted)
+        {
+            return unsorted;
         }
     }
 }
---
+++ test01Tests/SorterTests.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using test01;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test01.Tests
+{
+    [TestClass()]
+    public class SorterTests
+    {
+        [TestMethod()]
+        public void SortTest()
+        {
+            try
+            {
+                Sorter.Sort(null);
+                Assert.Fail("Should throw Argument Exception");
+            }
+            catch (ArgumentException ex)
+            {
+
+            }
+
+        }
+        [TestMethod()]
+        public void Test01()
+        {
+            try
+            {
+                var res = Sorter.Sort(new List<string[]>());
+                Assert.IsNotNull(res);
+                Assert.IsTrue(res.Count == 0);
+            }
+            catch (ArgumentException ex)
+            {
+                Assert.Fail("Should not throw Argument Exception");
+            }
+        }
+        [TestMethod()]
+        public void SortTest02()
+        {
+            List<string[]> unsorted = new List<string[]> {
+                new string[] { "Мельбурн", "Кельн" },
+                new string[] { "Москва", "Париж" },
+                new string[] { "Кельн", "Москва" }};
+            List<string[]> sorted = Sorter.Sort(unsorted);
+            Assert.IsTrue(sorted.Count == 3);
+            Assert.IsTrue(sorted[0][0] == "Мельбурн");
+            Assert.IsTrue(sorted[1][0] == "Кельн");
+            Assert.IsTrue(sorted[2][0] == "Москва");
+        }
+    }
+}
